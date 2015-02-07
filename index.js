6c2dd2493297dfd6f0b2eb5e1e0e2931585142bd
Emersion <contact@emersion.fr>
2015-02-07 21:34:38 +0100

    Adds waterline as DB backend

--- index.js
+++ index.js
@@ -2,8 +2,110 @@ var url = require('url');
 var express = require('express');
 var RSS = require('rss');
 var ATOM = require('./lib/atom');
+var Waterline = require('waterline');
 var config = require('./config');
 
+// ORM
+
+// Instantiate a new instance of the ORM
+var orm = new Waterline();
+
+var diskAdapter = require('sails-disk');
+var memoryAdapter = require('sails-memory');
+
+var ormConfig = {
+	// Setup Adapters
+	// Creates named adapters that have have been required
+	adapters: {
+		'default': memoryAdapter,
+		disk: diskAdapter,
+		memory: memoryAdapter
+	},
+	// Build Connections Config
+	// Setup connections using the named adapter configs
+	connections: {
+		disk: {
+			adapter: 'disk'
+		},
+		memory: {
+			adapter: 'memory'
+		}
+	},
+	defaults: {
+		migrate: 'alter'
+	}
+};
+
+var Feed = Waterline.Collection.extend({
+	identity: 'feed',
+	connection: 'memory',
+	attributes: {
+		slug: {
+			type: 'string',
+			index: true
+		},
+		title: 'string',
+		summary: 'string',
+		url: 'string',
+		image: 'string',
+		author: 'string',
+		rights: 'string',
+		language: 'string',
+		categories: 'array',
+		items: {
+			collection: 'feeditem',
+			via: 'feed'
+		}
+	}
+});
+
+var FeedItem = Waterline.Collection.extend({
+	identity: 'feeditem',
+	connection: 'memory',
+	attributes: {
+		slug: {
+			type: 'string',
+			index: true
+		},
+		title: 'string',
+		subtitle: 'string',
+		summary: 'string',
+		content: 'string',
+		url: 'string',
+		image: 'string',
+		authors: 'array',
+		contributors: 'array',
+		rights: 'string',
+		language: 'string',
+		categories: 'array',
+		feed: {
+			model: 'feed'
+		},
+		enclosures: {
+			collection: 'feedenclosure',
+			via: 'item'
+		}
+	}
+});
+
+var FeedEnclosure = Waterline.Collection.extend({
+	identity: 'feedenclosure',
+	connection: 'memory',
+	attributes: {
+		url: 'string',
+		type: 'string',
+		size: 'int',
+		item: {
+			model: 'feeditem'
+		}
+	}
+});
+
+orm.loadCollection(Feed);
+orm.loadCollection(FeedItem);
+orm.loadCollection(FeedEnclosure);
+
+// App
 var port = process.env.PORT || config.port || 9090;
 var baseUrl = config.base || '';
 
@@ -18,102 +120,74 @@ if (baseUrl) {
 	app.use(url.parse(baseUrl).pathname, router);
 }
 
-function indexFeed(Feed) {
-	var filename = (Feed === ATOM) ? 'atom' : 'rss';
+function renderFeedsFeed(feeds, Xml) {
+	var filename = (Xml === ATOM) ? 'atom' : 'rss';
 
-	var feed = new Feed({
+	var feed = new Xml({
 		title: 'LibreCast',
 		feed_url: baseUrl+'/'+filename+'.xml',
 		site_url: baseUrl,
 		generator: 'LibreCast'
 	});
 
-	var item = {
-		title: 'LibreCast channel',
-		description: 'LibreCast test channel',
-		url: baseUrl+'/librecast/',
-		date: '05 October 2011 14:48 UTC'
-	};
-	if (Feed === ATOM) {
-		item.custom_elements = [
-			{ link: { _attr: { type: 'application/atom+xml', rel: 'alternate', href: baseUrl+'/librecast/atom.xml' } } }
-		];
-	}
-	feed.item(item);
+	feeds.forEach(function (f) {
+		var item = {
+			title: f.title,
+			description: f.summary,
+			url: baseUrl+'/'+f.slug+'/',
+			date: f.createdAt
+		};
+		if (Xml === ATOM) {
+			item.custom_elements = [
+				{ link: { _attr: { type: 'application/atom+xml', rel: 'alternate', href: baseUrl+'/'+f.slug+'/atom.xml' } } }
+			];
+		}
+		feed.item(item);
+	});
 
 	return feed;
 }
 
-function channelFeed(Feed, channel) {
-	var filename = (Feed === ATOM) ? 'atom' : 'rss';
+function renderItemsFeed(model, items, Xml) {
+	var filename = (Xml === ATOM) ? 'atom' : 'rss';
 
-	var feed = new Feed({
-		title: 'LibreCast channel',
-		feed_url: baseUrl+'/'+channel+'/'+filename+'.xml',
-		site_url: baseUrl+'/'+channel+'/',
-		author: 'LibreCast',
+	var feed = new Xml({
+		title: model.title,
+		feed_url: baseUrl+'/'+model.slug+'/'+filename+'.xml',
+		site_url: baseUrl+'/'+model.slug+'/',
+		author: model.author,
 		generator: 'LibreCast'
 	});
 
-	var item = {
-		title: 'Hello World!',
-		description: 'First post',
-		url: baseUrl+'/'+channel+'/#hello-world',
-		date: '05 October 2011 14:48 UTC',
-		enclosure: {
-			url: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Big_Buck_Bunny_Trailer_400p.ogg',
-			size: '220514438',
-			type: 'video/ogg'
+	items.forEach(function (it) {
+		var item = {
+			title: it.title,
+			description: it.summary,
+			url: baseUrl+'/'+it.slug+'/',
+			date: it.createdAt
+		};
+
+		if (it.enclosures) {
+			var enc = it.enclosures[0];
+
+			item.enclosure = {
+				url: enc.url,
+				size: enc.size,
+				type: enc.type
+			};
 		}
-	};
-	if (Feed === ATOM) {
-		item.custom_elements = [
-			{ content: [
-				{ _attr: { type: 'xhtml' } },
-				{ div: [
-					{ _attr: { xmlns: 'http://www.w3.org/1999/xhtml' } },
-					{ p: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.' }
+		if (Xml === ATOM && it.content) {
+			item.custom_elements = [
+				{ content: [
+					{ _attr: { type: 'xhtml' } },
+					{ div: [
+						{ _attr: { xmlns: 'http://www.w3.org/1999/xhtml' } },
+						{ p: it.content }
+					] }
 				] }
-			] }
-		];
-	}
-	feed.item(item);
-
-	feed.item({
-		title: 'Nyan Cat',
-		description: 'LOL',
-		url: baseUrl+'/'+channel+'/#nyan-cat',
-		date: '05 October 2011 14:48 UTC',
-		enclosure: {
-			url: 'https://upload.wikimedia.org/wikipedia/en/2/2a/Nyan_cat.ogg',
-			size: '220514438',
-			type: 'audio/ogg'
+			];
 		}
-	});
-
-	feed.item({
-		title: 'Temple',
-		description: 'A random Mediawiki picture',
-		url: baseUrl+'/'+channel+'/#temple',
-		date: '05 October 2011 14:48 UTC',
-		enclosure: {
-			url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Fifty_Five_Window_Palace_np_GP_%2810%29.JPG/1024px-Fifty_Five_Window_Palace_np_GP_%2810%29.JPG',
-			size: '220514438',
-			type: 'image/jpg'
-		}
-	});
-
-	feed.item({
-		title: 'Torrent file',
-		description: 'Wow',
-		url: baseUrl+'/'+channel+'/#torrent-file',
-		date: '05 October 2011 14:48 UTC',
-		enclosure: {
-			//url: 'magnet:?xt=urn:btih:4A5942DD1BB1DF3D2491B18FF48F627415E1947C&dn=the+interview+2014+720p+brrip+x264+yify&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce',
-			url: 'http://torcache.net/torrent/4A5942DD1BB1DF3D2491B18FF48F627415E1947C.torrent?title=%5Bkickass.so%5Dthe.interview.2014.720p.brrip.x264.yify',
-			size: '123456',
-			type: 'application/x-bittorrent' // For .torrent files
-		}
+		feed.item(item);
 	});
 
 	return feed;
@@ -130,41 +204,197 @@ function sendFeed(res, feed) {
 
 router.use('/app/', express.static(__dirname+'/public'));
 
-router.get('/', function (req, res) {
-	var feed = indexFeed(ATOM);
-	res.render('index', { feed: feed });
-});
-router.get('/rss.xml', function (req, res) {
-	var feed = indexFeed(RSS);
-	sendFeed(res, feed);
-});
-router.get('/atom.xml', function (req, res) {
-	var feed = indexFeed(ATOM);
-	sendFeed(res, feed);
-});
+function populateDb(app) {
+	app.models.feed.create({
+		slug: 'librecast',
+		title: 'LibreCast channel',
+		summary: 'A simple test channel',
+		author: 'LibreCast'
+	}).exec(function (err, feed) {
+		if (err) throw err;
 
-router.get('/:channel/', function (req, res) {
-	var channel = req.params.channel;
+		app.models.feeditem.create({
+			slug: 'big-buck-bunny-trailer',
+			title: 'Big Buck Bunny trailer',
+			summary: 'A very fun movie',
+			content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.',
+			feed: feed.id
+		}).exec(function (err, item) {
+			if (err) throw err;
 
-	var feed = channelFeed(ATOM, channel);
-	res.render('channel', { feed: feed });
-});
-router.get('/:channel/rss.xml', function (req, res) {
-	var channel = req.params.channel;
+			app.models.feedenclosure.create({
+				url: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Big_Buck_Bunny_Trailer_400p.ogg',
+				type: 'video/ogg',
+				item: item.id
+			}).exec(function (err, feed) {
+				if (err) throw err;
+			});
+		});
 
-	var feed = channelFeed(RSS, channel);
-	sendFeed(res, feed);
-});
-router.get('/:channel/atom.xml', function (req, res) {
-	var channel = req.params.channel;
+		app.models.feeditem.create({
+			slug: 'nyan-cat',
+			title: 'Nyan Cat',
+			summary: 'LOL',
+			feed: feed.id
+		}).exec(function (err, item) {
+			if (err) throw err;
 
-	var feed = channelFeed(ATOM, channel);
-	sendFeed(res, feed);
-});
+			app.models.feedenclosure.create({
+				url: 'https://upload.wikimedia.org/wikipedia/en/2/2a/Nyan_cat.ogg',
+				type: 'audio/ogg',
+				item: item.id
+			}).exec(function (err, feed) {
+				if (err) throw err;
+			});
+		});
 
-var server = app.listen(port, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+		app.models.feeditem.create({
+			slug: 'temple',
+			title: 'Temple',
+			summary: 'A random Mediawiki picture',
+			feed: feed.id
+		}).exec(function (err, item) {
+			if (err) throw err;
 
-	console.log('Server listening at http://%s:%s', host, port);
+			app.models.feedenclosure.create({
+				url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Fifty_Five_Window_Palace_np_GP_%2810%29.JPG/1024px-Fifty_Five_Window_Palace_np_GP_%2810%29.JPG',
+				type: 'image/jpeg',
+				item: item.id
+			}).exec(function (err, feed) {
+				if (err) throw err;
+			});
+		});
+
+		app.models.feeditem.create({
+			slug: 'the-interview',
+			title: 'The Interview',
+			summary: 'Wow',
+			feed: feed.id
+		}).exec(function (err, item) {
+			if (err) throw err;
+
+			app.models.feedenclosure.create({
+				//url: 'magnet:?xt=urn:btih:4A5942DD1BB1DF3D2491B18FF48F627415E1947C&dn=the+interview+2014+720p+brrip+x264+yify&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce',
+				url: 'http://torcache.net/torrent/4A5942DD1BB1DF3D2491B18FF48F627415E1947C.torrent?title=%5Bkickass.so%5Dthe.interview.2014.720p.brrip.x264.yify',
+				type: 'application/x-bittorrent',
+				item: item.id
+			}).exec(function (err, feed) {
+				if (err) throw err;
+			});
+		});
+	});
+}
+
+function setupApi(app) {
+	var router = express.Router();
+
+	router.get('/', function (req, res) {
+		res.json({
+			feeds_url: baseUrl+'/api/feeds'
+		});
+	});
+
+	router.get('/feeds', function (req, res) {
+		app.models.feed.find().exec(function (err, models) {
+			if (err) return res.json({ err: err }, 500);
+
+			models.forEach(function (model) {
+				model.url = baseUrl+'/api/feeds/'+model.slug;
+			});
+
+			res.json(models);
+		});
+	});
+
+	router.get('/feeds/:slug', function (req, res) {
+		app.models.feed.findOne({ slug: req.params.slug }, function (err, feed) {
+			if (err) return res.json({ err: err }, 500);
+			app.models.feeditem.find({ feed: feed.id }).populate('enclosures').exec(function (err, models) {
+				if (err) return res.json({ err: err }, 500);
+				res.json(models);
+			});
+		});
+	});
+
+	app.use('/api', router);
+}
+
+function setupFeeds(app) {
+	app.get('/rss.xml', function (req, res) {
+		app.models.feed.find().exec(function (err, models) {
+			if (err) return res.json({ err: err }, 500);
+			var feed = renderFeedsFeed(models, RSS);
+			sendFeed(res, feed);
+		});
+	});
+	app.get('/atom.xml', function (req, res) {
+		app.models.feed.find().exec(function (err, models) {
+			if (err) return res.json({ err: err }, 500);
+			var feed = renderFeedsFeed(models, ATOM);
+			sendFeed(res, feed);
+		});
+	});
+
+	app.get('/:channel/rss.xml', function (req, res) {
+		app.models.feed.findOne({ slug: req.params.channel }, function (err, model) {
+			if (err) return res.json({ err: err }, 500);
+			app.models.feeditem.find({ feed: model.id }).populate('enclosures').exec(function (err, models) {
+				if (err) return res.json({ err: err }, 500);
+				var feed = renderItemsFeed(model, models, RSS);
+				sendFeed(res, feed);
+			});
+		});
+	});
+	app.get('/:channel/atom.xml', function (req, res) {
+		app.models.feed.findOne({ slug: req.params.channel }, function (err, model) {
+			if (err) return res.json({ err: err }, 500);
+			app.models.feeditem.find({ feed: model.id }).populate('enclosures').exec(function (err, models) {
+				if (err) return res.json({ err: err }, 500);
+				var feed = renderItemsFeed(model, models, ATOM);
+				sendFeed(res, feed);
+			});
+		});
+	});
+}
+
+function setupSite(app) {
+	app.get('/', function (req, res) {
+		app.models.feed.find().exec(function (err, models) {
+			if (err) return res.json({ err: err }, 500);
+
+			res.render('index', { feeds: models });
+		});
+	});
+
+	app.get('/:channel/', function (req, res) {
+		app.models.feed.findOne({ slug: req.params.channel }, function (err, model) {
+			if (err) return res.json({ err: err }, 500);
+			app.models.feeditem.find({ feed: model.id }).populate('enclosures').exec(function (err, models) {
+				if (err) return res.json({ err: err }, 500);
+				res.render('channel', { feed: model, items: models });
+			});
+		});
+	});
+}
+
+orm.initialize(ormConfig, function (err, models) {
+	if (err) throw err;
+
+	(function (app) {
+		app.models = models.collections;
+		app.connections = models.connections;
+
+		setupApi(app);
+		setupFeeds(app);
+		setupSite(app);
+
+		populateDb(app);
+	})(router);
+
+	var server = app.listen(port, function () {
+		var host = server.address().address;
+		var port = server.address().port;
+
+		console.log('Server listening at http://%s:%s', host, port);
+	});
 });
